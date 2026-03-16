1bf01bf4103ec48890e65b4badbd4ec78d388224
Orion <orion1030.tr@gmail.com>
2026-03-15 20:26:20 -0500

    refactor: remove JWT_SECRET validation from server.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
-if (!process.env.JWT_SECRET) {
-  throw new Error("JWT_SECRET must be set in the environment (e.g. in Railway Variables).");
-}
+
 const { Environments } = require("./utils/constants");
 const serverless = require("serverless-http");
 const { DBConnection, UserModel } = require("./dbModels");
